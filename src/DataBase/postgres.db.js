44dcaf4a2cb1313b86b0ea7228e28a6ec1615dc4
William Ferreira <55250304+tecwilliam38@users.noreply.github.com>
2025-05-16 17:39:09 -0300

    teste

--- src/DataBase/postgres.db.js
+++ src/DataBase/postgres.db.js
@@ -6,7 +6,7 @@ const { Pool } = pg;
 // JWT xFW6e3bktVP5k4Lr+5BmqKyHdWjWyDugRBk4uFnhO8FaK6tffi4TZ9wuujMIEvlNGbTa0DB+IrQ8crJ0EW8vPA==
 // ID kpcanhcozznqvfibklhd
 // API key eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtwY2FuaGNvenpucXZmaWJrbGhkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU4MjQ5OTQsImV4cCI6MjA1MTQwMDk5NH0.684EaApGs1HDiRVK3G04SFSvZxbqGaN2orbdWq31D7E
-// apa
+// apa fgfgf
 const pool = new Pool({    
     user: 'postgres.kpcanhcozznqvfibklhd',
     host: 'aws-0-sa-east-1.pooler.supabase.com',
